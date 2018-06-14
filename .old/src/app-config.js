254c48aa2970ed8eaf12cb54e3143187bed631f9
Jan Chalupa <janchalupa@outlook.cz>
2018-06-14 22:36:24 +0200

    no message

--- .old/src/app-config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'Jan Chalupa | Portfolio',
-    nameShort: 'Portfolio',
-    description: 'Welcome, this is my portfolio...',
-    accentColor: '#ff5252',
-    caching: {
-        strategy: 'staleWhileRevalidate'
-    }
-};
