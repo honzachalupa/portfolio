814d9860838a125aed1f2a09ebd3c8a5c96caf1a
Jan Chalupa <janchalupa@outlook.cz>
2023-02-01 16:06:43 +0100

    fix

--- src/pages/administration/index.tsx
+++ src/pages/administration/index.tsx
@@ -1,6 +1,5 @@
 import { useRouter } from "next/router";
 import { useEffect } from "react";
-import { ProjectActions } from "../../actions/project";
 import { ProjectsList } from "../../components/admin/ProjectsList";
 import { useSupabaseAuth } from "../../hooks/useSupabaseAuth";
 import { LayoutPrimary as Layout } from "../../layouts/Primary";
@@ -8,13 +7,6 @@ import { LayoutPrimary as Layout } from "../../layouts/Primary";
 export default function Administration() {
     const router = useRouter();
     const { user, isLoading } = useSupabaseAuth();
-
-    const createProject = () => {
-        ProjectActions.create({
-            id: "id",
-            name: "Sample value.",
-        });
-    };
 
     useEffect(() => {
         if (!user && !isLoading) {
