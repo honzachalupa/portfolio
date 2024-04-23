756545cd637538ddc1db9ff112e65ba5f6506d6d
Jan Chalupa <janchalupa@outlook.cz>
2024-04-23 17:58:33 +0200

    fix: Fixed jobs sorting.

--- src/components/cms/blocks/Jobs.tsx
+++ src/components/cms/blocks/Jobs.tsx
@@ -4,11 +4,19 @@ import { SectionContainer } from "../layouts/Primary";
 import { ViewDesktop } from "./Jobs.desktop";
 import { ViewMobile } from "./Jobs.mobile";
 
-export const Block_Jobs: React.FC<IBlock_Jobs> = ({ headline, jobs }) => (
-    <AnimationFadeIn>
-        <SectionContainer headline={headline}>
-            <ViewDesktop jobs={jobs} />
-            <ViewMobile jobs={jobs} />
-        </SectionContainer>
-    </AnimationFadeIn>
-);
+export const Block_Jobs: React.FC<IBlock_Jobs> = ({ headline, jobs }) => {
+    const jobsSorted = [...jobs].sort((a, b) => {
+        if (a.dateTo < b.dateTo) return 1;
+        if (a.dateTo > b.dateTo) return -1;
+        return 0;
+    });
+
+    return (
+        <AnimationFadeIn>
+            <SectionContainer headline={headline}>
+                <ViewDesktop jobs={jobsSorted} />
+                <ViewMobile jobs={jobsSorted} />
+            </SectionContainer>
+        </AnimationFadeIn>
+    );
+};
