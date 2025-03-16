a857cd9a69729392fe9ecc28fbb0b7150d813fe4
Jan Chalupa <janchalupa@outlook.cz>
2025-03-16 20:13:51 +0100

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,13 @@
 import hygraph from "@/actions/hygraph";
 import { ContentRenderer } from "@/components/ContentRenderer";
 import type { Metadata } from "next";
-import { cache } from "react";
 
 export const metadata: Metadata = {
   title: "Jan Chalupa portfolio",
 };
 
-export const preload = (): void => {
-  void hygraph.getPage("/");
-};
-
-const fetchPage = cache(async () => {
-  const data = await hygraph.getPage("/");
-
-  return data;
-});
-
 export default async function Page(): Promise<React.ReactNode> {
-  const page = await fetchPage();
+  const page = await hygraph.getPage("/");
 
   if (!page) {
     throw new Error("Page not found");
