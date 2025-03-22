69559c4176621fcbfc418e71c4da8bce588a0e46
Jan Chalupa <janchalupa@outlook.cz>
2025-03-22 13:43:53 +0100

    chore: WIP.

--- app/manifest.ts
+++ app/manifest.ts
@@ -6,16 +6,21 @@ export default async function manifest(): Promise<MetadataRoute.Manifest> {
 
   return {
     name: config?.seo?.name,
-    short_name: config?.seo?.name,
+    short_name: config?.seo?.name.replace("portfolio", "").trim(),
     description: config?.seo?.description,
     start_url: "/",
     display: "standalone",
-    background_color: "#ffffff",
-    theme_color: "#000000",
+    background_color: "#000",
+    theme_color: "#FFF",
     icons: [
       {
-        src: "/icon-192x192.png",
+        src: "/android-chrome-192x192.png",
         sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/android-chrome-512x512.png",
+        sizes: "512x512",
         type: "image/png",
       },
     ],
