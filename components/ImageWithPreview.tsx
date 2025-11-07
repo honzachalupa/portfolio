869803cc1efb2b92496bc6358a91d1dbc76bd743
Jan Chalupa <janchalupa@outlook.cz>
2025-11-07 13:18:12 +0100

    Fixed screenshots sizing.

--- components/ImageWithPreview.tsx
+++ components/ImageWithPreview.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { AppleAppStoreScreenshot } from "@/app/api/apple-app-store/route";
-import { Button } from "@heroui/button";
 import { Modal, ModalContent, useDisclosure } from "@heroui/modal";
 import clsx from "clsx";
 import Image from "next/image";
@@ -12,26 +11,34 @@ interface ImageWithPreviewProps {
   className?: string;
 }
 
-export function ImageWithPreview({ image, alt, className }: ImageWithPreviewProps): React.ReactNode {
+export function ImageWithPreview({
+  image,
+  alt,
+  className,
+}: ImageWithPreviewProps): React.ReactNode {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
   return (
     <>
-      <Button className={clsx("p-0", className)} onPress={onOpen}>
-        <div className="h-full">
-          <Image
-            src={image.url}
-            alt={alt}
-            width={image.width / 10}
-            height={image.height / 10}
-            className="w-full h-full"
-          />
-        </div>
-      </Button>
+      <div className={clsx("cursor-pointer overflow-hidden", className)} onClick={onOpen}>
+        <Image
+          src={image.url}
+          alt={alt}
+          width={image.width}
+          height={image.height}
+          className="h-full w-auto object-cover"
+        />
+      </div>
 
       <Modal backdrop="blur" size="5xl" isOpen={isOpen} onOpenChange={onOpenChange}>
         <ModalContent className="p-0 pt-10 w-fit h-fit">
-          <Image src={image.url} alt={alt} width={image.width} height={image.height} className="max-h-[80vh] w-auto" />
+          <Image
+            src={image.url}
+            alt={alt}
+            width={image.width}
+            height={image.height}
+            className="max-h-[80vh] w-auto"
+          />
         </ModalContent>
       </Modal>
     </>
